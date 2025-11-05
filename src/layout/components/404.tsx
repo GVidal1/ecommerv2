2ef677b0a7f92b86af576597f5f1437778388778
Riskoder <g.vidal1g+1@gmail.com>
2025-11-05 13:40:38 -0300

    Se cambiaron los nombres de las carpetas

--- src/layout/components/404.tsx
+++ src/layout/components/404.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <div style={{ textAlign: 'center' }}>
+  <div className="notFound">
     <h2>404 - Página No Encontrada</h2>
     <p>Lo sentimos, la página que buscas no existe.</p>
     <Link to="/">Volver al Home</Link>
