81a2a357cb735737fa2985d7c4b342a6bd4b4471
Riskoder <g.vidal1g+1@gmail.com>
2025-11-05 18:47:06 -0300

    Se creo un index para el typado global

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  thumbnail: string;
+  description: string;
+  category: string;
+  images: string[];
+  discountPercentage: number;
+}
+
+export interface User {
+  email: string;
+  password: string;
+  nombre: string;
+  rol: 'admin' | 'user';
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
